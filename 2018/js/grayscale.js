0bfd9ef9361932a6c42e3b12fbf470e25cddcb10
hrambelo <bensmania@gmail.com>
2018-11-13 07:18:26 +0100

    add 2018 souvenir
    Former-commit-id: 5ec6330f800042ced88ddbcabfdeef7915cc5071

---
+++ 2018/js/grayscale.js
@@ -0,0 +1,25 @@
+//jQuery to collapse the navbar on scroll
+$(window).scroll(function() {
+    if ($(".navbar").offset().top > 50) {
+        $(".navbar-fixed-top").addClass("top-nav-collapse");
+    } else {
+        $(".navbar-fixed-top").removeClass("top-nav-collapse");
+    }
+});
+
+//jQuery for page scrolling feature - requires jQuery Easing plugin
+$(function() {
+    $('.page-scroll a').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault();
+    });
+});
+$(function () {
+    jQuery('#flipcountdownbox').flipcountdown({  
+        size:'sm',
+        beforeDateTime:'06/28/2018 00:00:01'
+    });
+});
